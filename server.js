45e0566721829228fdeca72131bdd28315301f56
imsranka <ranka.sakshi9@example.com>
2020-04-06 10:02:50 +0530

    Adding response

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var multer = require('multer');
 var bodyParser = require('body-parser');
 var Datauri = require('datauri');
-var path = require('path');
 var express = require('express');
 var cloudinary = require('cloudinary').v2;
+var cors = require('cors');
 
 const storage = multer.memoryStorage();
 const multerUploads = multer ({ storage }).single('image');
@@ -16,30 +16,42 @@ cloudinary.config({
 });
 
 const app =express();
+app.use(cors());
 app.use(bodyParser.json()).use(bodyParser.urlencoded({extended:false}));
 
 app.post('/upload',multerUploads, (req, res) => {
-   // console.log('req.file:',req.file.mimetype);str.slice(str.indexOf("/")+1)
-   const mime=req.file.mimetype;
-   const ext= mime.slice(mime.indexOf('/')+1);
-   //console.log(req.file);
+     
+  if(req.file){
+  //console.log(req.file);
+    const mime=req.file.mimetype;
+    const ext= mime.slice(mime.indexOf('/')+1);
+
+    //Converting buffer content to a uri
     const dataUri = req => {
-       return dUri.format(ext.toString(), req.file.buffer);
-    
+      return dUri.format(ext.toString(), req.file.buffer); 
     }
 
     const file = dataUri(req).content;
     console.log(file);
 
     cloudinary.uploader.upload(file)
-    .then(res=>console.log(res))
-    .catch(err=>console.log(err))
-    
-    
-    //res.send(req.file);
-    
-      // const file = dataUri(req).content;
-      // console.log(file);
+    .then(result=> {
+      console.log(result);
+      console.log(result.url);
+      const imageurl = result.url;
+      return res.status(200).json({
+        message: 'Upload Successful',
+        data: {imageurl}
+      })    
+    })
+    .catch(err=> {
+      console.log(err);
+      res.status(400).json({
+        message: 'Someting went wrong while processing your request',
+        data: {err}
+      })
+    })
+  }
     
 });
 
